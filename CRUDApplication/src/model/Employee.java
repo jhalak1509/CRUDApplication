0c1d3b5c0ca5cfe48a533ec4c9779a86c114ab65
jhalak1509 <surve.j@northeastern.edu>
2022-10-01 17:24:16 -0400

    Added Employee class, defined variables, added getters and setters for all variables

--- CRUDApplication/src/model/Employee.java
+++ CRUDApplication/src/model/Employee.java
@@ -10,4 +10,95 @@ package model;
  */
 public class Employee {
     
+    private String employeeName;
+    private int employeeId;
+    private int age;
+    private char gender;
+    private String startDate;
+    private String level;
+    private String team;
+    private String position;
+    private long cellNumber;
+    private String email;
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public long getCellNumber() {
+        return cellNumber;
+    }
+
+    public void setCellNumber(long cellNumber) {
+        this.cellNumber = cellNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
 }
