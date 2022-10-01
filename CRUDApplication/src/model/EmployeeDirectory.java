11cabb526b7568e6c2923a16b8a379705fe64bd9
jhalak1509 <surve.j@northeastern.edu>
2022-10-01 17:34:21 -0400

    Added class EmployeeDirectory for storing collection of Employees

---
+++ CRUDApplication/src/model/EmployeeDirectory.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jhalaksurve
+ */
+public class EmployeeDirectory {
+    
+    private ArrayList<Employee> directory;
+    
+    public EmployeeDirectory(){
+    
+    this.directory = new ArrayList<Employee>();
+    }
+
+    public ArrayList<Employee> getDirectory() {
+        return directory;
+    }
+
+    public void setDirectory(ArrayList<Employee> directory) {
+        this.directory = directory;
+    }
+}
